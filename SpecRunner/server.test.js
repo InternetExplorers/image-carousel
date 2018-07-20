49be89f325adf296e00a0646dde92c48925d941f
Shane Yuan <shaneyuan18@gmail.com>
2018-07-20 15:12:02 -0700

    Set up server and server tests

--- SpecRunner/server.test.js
+++ SpecRunner/server.test.js
@@ -1,5 +1,18 @@
-const server = require('../server/index');
+const mysql = require('mysql');
+const request = require('request');
+// const server = require('../server/index');
 
-test('adds 2 to 1 to equal 3', () => {
-  expect(server.test(1)).toBe(3);
+// test('adds 2 to 1 to equal 3', () => {
+//   expect(server.test(1)).toBe(3);
+// });
+
+describe('Server database connection test', () => {
+  it('Allow get request on images API', (done) => {
+    request('http://localhost:3001/businesses/1/images', (error, response, body) => {
+      const image = JSON.parse(body);
+      expect(image[0].title).toEqual('food');
+      expect(image[0].description).toEqual('this is good');
+      done();
+    });
+  });
 });
--- server/index.js
+++ server/index.js
@@ -1,3 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const db = require('../database/index');
+
+const app = express();
+const port = 3001;
+
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, '/../client/dist')));
+
+app.get('/businesses/:businessId/images', (req, res) => {
+  const { businessId } = req.params;
+  const queryString = `SELECT businesses.name,
+    users.name,
+    users.imageUrl,
+    images.id,
+    images.title,
+    images.description,
+    images.date,
+    images.originalUrl,
+    images.thumbnailUrl
+    from businesses, images, users where businesses.id = ${businessId} AND
+    users.id = images.ownerId;`;
+
+  db.query(queryString, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
+
+
 module.exports = {
   test: number => number + 2,
 };
