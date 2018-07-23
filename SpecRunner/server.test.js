61d95daaf165990691bb09fcfa0a2e7fd142ede3
Shane Yuan <shaneyuan18@gmail.com>
2018-07-23 00:54:42 -0700

    update test

--- SpecRunner/server.test.js
+++ SpecRunner/server.test.js
@@ -4,8 +4,8 @@ describe('Server database connection test', () => {
   it('Allow get request on images API', (done) => {
     request('http://localhost:3001/businesses/1/images', (error, response, body) => {
       const image = JSON.parse(body);
-      expect(image[0].title).toEqual('food');
-      expect(image[0].description).toEqual('this is good');
+      expect(image[0].title).toEqual('raw shrimp');
+      expect(image[0].description).toEqual('this is bad');
       done();
     });
   });
