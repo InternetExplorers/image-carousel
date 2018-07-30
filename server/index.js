225f9745ba3f96fd7254803c33541ab87845ccbe
Shane Yuan <shaneyuan18@gmail.com>
2018-07-30 16:33:59 -0700

    Update server and webpack

--- server/index.js
+++ server/index.js
@@ -7,6 +7,11 @@ const app = express();
 const port = 3001;
 
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(express.static(path.join(__dirname, '/../client/dist')));
 
