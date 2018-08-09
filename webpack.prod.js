6ea879bff2a5958a408d1e83bf6ad53ee71c92eb
Michael Chan <mikey.chan1@gmail.com>
2018-08-09 14:08:11 -0700

    revert previous commit

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,12 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.config.js');
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, '/client/src');
+const DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
+  // entry: `${SRC_DIR}/components/ImageCarousel.jsx`,
 });
