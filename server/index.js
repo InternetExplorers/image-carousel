c954d8a37303d7966b87630d9a8867834da9b0df
Michael Chan <mikey.chan1@gmail.com>
2018-08-09 14:10:37 -0700

    add CRUD routes

--- server/index.js
+++ server/index.js
@@ -2,43 +2,62 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const db = require('../database/index');
+const dbHelper = require('../database/dbHelpers.js');
 
 const app = express();
 const port = 3001;
 
 app.use(bodyParser.json());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
-app.use(express.static(path.join(__dirname, '/../client/dist')));
+app.use('/:id/', express.static(path.join(__dirname, '/../client/dist')));
 
 app.get('/businesses/:businessId/images', (req, res) => {
-  const { businessId } = req.params;
-  const queryString = `SELECT businesses.name,
-    users.profileName,
-    users.profileUrl,
-    users.profileReviewCount,
-    users.profileFriendCount,
-    images.id,
-    images.title,
-    images.description,
-    images.date,
-    images.originalUrl,
-    images.thumbnailUrl
-    from businesses, images, users where businesses.id = ${businessId} AND
-    images.businessId =  ${businessId} AND
-    users.id = images.ownerId;`;
-
-  db.query(queryString, (err, result) => {
+  dbHelper.getImages({ id: req.params.businessId }, (err, images) => {
     if (err) {
-      res.send(err);
+      res.status(400).send(err);
     } else {
-      res.send(result);
+      res.status(200).send(images);
     }
   });
+
+});
+
+app.post('/businesses/:businessId/images', (req, res) => {
+  dbHelper.postImage({ id: req.params.businessId }, (err, data) => {
+    if (err) {
+      res.status(400).send(err);
+    } else {
+      res.status(200).send();
+    }
+  });
+});
+
+app.put('/businesses/:businessId/images/:imageId', (req, res) => {
+  const { businessId, imageId } = req.params;
+  dbHelper.updateImage((err, data) => {
+    if (err) {
+      res.status(400).send(err);
+    } else {
+      res.status(200).send();
+    }
+  })
+});
+
+app.delete('/businesses/:businessId/images', (req, res) => {
+  const { businessId } = req.params;
+  dbHelper.deleteImage({ id: req.params.businessId }, (err, data) => {
+    if (err) {
+      res.status(400).send(err);
+    } else {
+      res.status(200).send();
+    }
+  })
 });
 
 app.listen(port, () => {
