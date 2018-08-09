61f5f801a8390ee9c5decdc634b4916236c85322
Michael Chan <mikey.chan1@gmail.com>
2018-08-09 14:11:34 -0700

    add dbhelpers for CRUD api

---
+++ database/dbHelpers.js
@@ -0,0 +1,55 @@
+
+const getImages = (data, cb) => {
+  const queryString = `SELECT businesses.name,
+    users.profileName,
+    users.profileUrl,
+    users.profileReviewCount,
+    users.profileFriendCount,
+    images.id,
+    images.title,
+    images.description,
+    images.date,
+    images.originalUrl,
+    images.thumbnailUrl
+    from businesses, images, users where businesses.id = ${businessId} AND
+    images.businessId = ${businessId} AND
+    users.id = images.ownerId;`;
+
+  db.query(queryString, (err, result) => {
+    if (err) cb(err);
+    else cb(result);
+  });
+}
+
+const postImage = (data, cb) => {
+  const queryStr = `INSERT INTO users (ownerId, businessId, title, description, date, originalUrl, thumbnailUrl) VALUES (?, ?, ?, ?)`;
+  const queryParams = [req.body.ownerId, businessId, req.body.title, req.body.description, req.body.date, req.body.originalUrl, req.body.thumbnailUrl];
+  db.query(queryStr, queryParams, (err, result) => {
+    if (err) cb (err);
+    else cb (data);
+  });
+
+};
+
+const updateImage = (data, cb) => {
+  const queryStr = `UPDATE images SET (ownerId, businessId, title, description, originalUrl, thumbnailUrl, date) = (?, ?, ?, ?, ?, ?, ?) WHERE images.id=${imageId}`;
+  const queryParams = [req.body.ownerId, req.body.businessId, req.body.title, req.body.description, req.body.originalUrl, req.body.thumbnailUrl, req.body.date];
+  db.query(queryStr, queryParams, (err, result) => {
+    if (err) {
+      cb (err);
+    } else {
+      cb (data);
+    }
+  });
+}
+
+const deleteImage = (data, cb) => {
+  
+}
+
+module.exports = {
+  getImages,
+  postImage,
+  updateImage,
+  deleteImage,
+}
