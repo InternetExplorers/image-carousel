98e7acb85681d57e0cd56fcb2160634c0f48d910
Michael Chan <mikey.chan1@gmail.com>
2018-08-10 14:36:29 -0700

    implement data generators

---
+++ database/bizDataGenerator.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const faker = require('faker');
+
+const adj = [
+  'Tasty',
+  'Gorgeous',
+  'Awesome',
+  'Unbranded',
+  'Handcrafted',
+  'Small',
+  'Incredible',
+  'Ergonomic',
+  'Refined',
+  'Licensed',
+];
+
+const append = (data) => {
+  const chunks = [];
+  let prevIdx = 0;
+  for (let j = 0; j <= data.length; j += 10000000) {
+    chunks.push(data.slice(prevIdx, j).join('\n'));
+    prevIdx = j;
+  }
+  // for (let i = 1; i <= 5; i += 1) {
+    const headers = 'index,business\n';
+    fs.appendFileSync(`./fakeBizData/data.csv`, headers + chunks);
+  // }
+};
+
+const makeUniqueBusinesses = (rounds) => {
+  const storage = [];
+  let id = 1;
+  const makeCombos = (counter, combo = []) => {
+    if (!counter) {
+      storage.push([id, combo.join(' ')]);
+      id++;
+    } else {
+      for (let i = 0; i < adj.length; i += 1) {
+        const element = adj[i];
+        makeCombos(counter - 1, combo.concat(element));
+      }
+    }
+  };
+  makeCombos(rounds);
+  append(storage);
+};
+
+makeUniqueBusinesses(7);
