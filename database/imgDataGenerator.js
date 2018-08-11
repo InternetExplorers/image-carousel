bf0f7a807ec80119bed6b416c6104c6905d58e8c
Michael Chan <mikey.chan1@gmail.com>
2018-08-11 14:32:52 -0700

    modify imgData to generate 50mil

--- database/imgDataGenerator.js
+++ database/imgDataGenerator.js
@@ -4,29 +4,29 @@ const faker = require('faker');
 const randomNumber = (min = 0, max = 9) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 const images = (num) => `https://s3-us-west-1.amazonaws.com/sdcfoodphotos/Food+Images/PIcture${num}.jpg`;
-// 2018-01-25
 
 const date = `2018-${randomNumber(0,12)}-${randomNumber(0,31)}`;
 
 const createImgs = () => {
-  const headers = 'index,ownerId,businessId,title,description,date,url\n';
+  const headers = 'index,ownerId,businessId,title,description,date,url,thumbnail\n';
   let count = 0;
-  while (count < 10000000) {
+  while (count < 50000000) {
     let storage = ``;
     let chunkNum = 0;
     if (count === 0) {
       storage += headers;
+      count++
     }
-    while (chunkNum < 100000) {
+    while (chunkNum < 200000) {
       let url = images(randomNumber(0, 10));
       //idx
       storage += `${count},`;
       //owner id
-      storage += `${randomNumber(0, 10000000)},`;
+      storage += `${randomNumber(1, 10000000)},`;
       //bus id
-      storage += `${randomNumber(0, 10000000)},`;
+      storage += `${randomNumber(1, 10000000)},`;
       //color, title
-      storage += `${faker.commerce.color()} ${faker.random.word()},`;
+      storage += `${faker.commerce.color()} ${faker.commerce.productName()},`;
       //description
       storage += `${faker.lorem.word()} ${faker.lorem.word()} ${faker.lorem.word()},`;
       //original url
@@ -41,7 +41,7 @@ const createImgs = () => {
       chunkNum += 1;
       count += 1;
     }
-    fs.appendFileSync(`fakeImgData/imgDataSample.csv`, storage);
+    fs.appendFileSync(`fakeImgData/fakeImgData.csv`, storage);
   }
 }
 
