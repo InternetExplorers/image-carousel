06217b56953bef3b7fe38cf8ed41e8ed969f7c70
Michael Chan <mikey.chan1@gmail.com>
2018-08-13 19:05:10 -0700

    create join table with postgres query for cassandra

--- database/postgreSQL/schema.sql
+++ database/postgreSQL/schema.sql
@@ -1,13 +1,12 @@
-
-DROP TABLE IF EXISTS businesses;
-CREATE TABLE  businesses (
+DROP TABLE IF EXISTS sample_businesses;
+CREATE TABLE  sample_businesses (
   id integer NOT NULL,
   name character varying(100),
   PRIMARY KEY (id)
 );
 
-DROP TABLE IF EXISTS users;
-CREATE TABLE users (
+DROP TABLE IF EXISTS sample_users;
+CREATE TABLE sample_users (
   id integer NOT NULL ,
   profilename character varying(50) NOT NULL,
   profileurl character varying(100),
@@ -16,8 +15,8 @@ CREATE TABLE users (
   PRIMARY KEY (id)
 );
 
-DROP TABLE IF EXISTS images;
-CREATE TABLE images (
+DROP TABLE IF EXISTS sample_images;
+CREATE TABLE sample_images (
   id serial NOT NULL,
   userId int NOT NULL,
   businessId int NOT NULL,
@@ -29,15 +28,20 @@ CREATE TABLE images (
   PRIMARY KEY (id)
 );
 
-COPY businesses FROM '/Users/michaelchan/Documents/Repos/W8 SDC/image-carousel/database/fakeBizData/data.csv' DELIMITER ',' CSV HEADER;
+COPY sample_businesses FROM '/Users/michaelchan/Documents/Repos/W8 SDC/image-carousel/database/fakeBizData/fakeBizSample.csv' DELIMITER ',' CSV HEADER;
 
-COPY users FROM '/Users/michaelchan/Documents/Repos/W8 SDC/image-carousel/database/fakeUserData/userData.csv' DELIMITER ',' CSV HEADER;
+COPY sample_users FROM '/Users/michaelchan/Documents/Repos/W8 SDC/image-carousel/database/fakeUserData/fakeUserDataSample.csv' DELIMITER ',' CSV HEADER;
 
-COPY images FROM '/Users/michaelchan/Documents/Repos/W8 SDC/image-carousel/database/fakeImgData/fakeImgData.csv' DELIMITER ',' CSV HEADER;
+COPY sample_images FROM '/Users/michaelchan/Documents/Repos/W8 SDC/image-carousel/database/fakeImgData/fakeImgDataSample.csv' DELIMITER ',' CSV HEADER;
 
 -- ALTER TABLE to add foreign keys
-ALTER TABLE images 
-  ADD FOREIGN KEY (userId) REFERENCES users(id);
-ALTER TABLE images 
-  ADD FOREIGN KEY (businessId) REFERENCES businesses(id);
+ALTER TABLE sample_images 
+  ADD FOREIGN KEY (userId) REFERENCES sample_users(id);
+ALTER TABLE sample_images 
+  ADD FOREIGN KEY (businessId) REFERENCES sample_businesses(id);
 
+--select (sample_images.id, sample_businesses.name, sample_users.profilename, sample_users.profileurl, sample_users.reviewcount, sample_users.friendcount, sample_images.title, sample_images.description, sample_images.url, sample_images.thumbnail, sample_images.date)
+
+-- COPY (select * from images
+-- inner join businesses on images.businessid=businesses.id 
+-- inner join users on images.userid=users.id) to '/Users/michaelchan/Documents/Repos/W8 SDC/image-carousel/database/cassandra/allData.csv' CSV DELIMITER ',' HEADER;
