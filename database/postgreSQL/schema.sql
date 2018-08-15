735a375278993cfc242431a3cc50c0a9090af5cc
Michael Chan <mikey.chan1@gmail.com>
2018-08-14 23:02:23 -0700

    create export file for cassandra

--- database/postgreSQL/schema.sql
+++ database/postgreSQL/schema.sql
@@ -1,13 +1,13 @@
-DROP TABLE IF EXISTS sample_businesses;
-CREATE TABLE  sample_businesses (
-  id integer NOT NULL,
-  name character varying(100),
-  PRIMARY KEY (id)
-);
+-- DROP TABLE IF EXISTS businesses;
+-- CREATE TABLE businesses (
+--   id serial,
+--   businessname character varying(100),
+--   PRIMARY KEY (id)
+-- ) ;
 
-DROP TABLE IF EXISTS sample_users;
-CREATE TABLE sample_users (
-  id integer NOT NULL ,
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  id serial,
   profilename character varying(50) NOT NULL,
   profileurl character varying(100),
   reviewcount integer,
@@ -15,9 +15,9 @@ CREATE TABLE sample_users (
   PRIMARY KEY (id)
 );
 
-DROP TABLE IF EXISTS sample_images;
-CREATE TABLE sample_images (
-  id serial NOT NULL,
+DROP TABLE IF EXISTS images;
+CREATE TABLE images (
+  id serial,
   userId int NOT NULL,
   businessId int NOT NULL,
   title varchar(75),
@@ -28,20 +28,57 @@ CREATE TABLE sample_images (
   PRIMARY KEY (id)
 );
 
-COPY sample_businesses FROM '/Users/michaelchan/Documents/Repos/W8 SDC/image-carousel/database/fakeBizData/fakeBizSample.csv' DELIMITER ',' CSV HEADER;
+-- COPY businesses (businessname) FROM '/Users/michaelchan/Documents/Repos/8WSDC/image-carousel/database/fakeBizData/data.csv' DELIMITER ',' CSV HEADER;
 
-COPY sample_users FROM '/Users/michaelchan/Documents/Repos/W8 SDC/image-carousel/database/fakeUserData/fakeUserDataSample.csv' DELIMITER ',' CSV HEADER;
+COPY users (profilename, profileurl, reviewcount, friendcount) FROM '/Users/michaelchan/Documents/Repos/8WSDC/image-carousel/database/fakeUserData/userData.csv' DELIMITER ',' CSV HEADER;
 
-COPY sample_images FROM '/Users/michaelchan/Documents/Repos/W8 SDC/image-carousel/database/fakeImgData/fakeImgDataSample.csv' DELIMITER ',' CSV HEADER;
+COPY images (userid, businessid, title, description, url, thumbnail, date) FROM '/Users/michaelchan/Documents/Repos/8WSDC/image-carousel/database/fakeImgData/fakeImgData.csv' DELIMITER ',' CSV HEADER;
 
 -- ALTER TABLE to add foreign keys
-ALTER TABLE sample_images 
-  ADD FOREIGN KEY (userId) REFERENCES sample_users(id);
-ALTER TABLE sample_images 
-  ADD FOREIGN KEY (businessId) REFERENCES sample_businesses(id);
+ALTER TABLE images 
+  ADD FOREIGN KEY (userId) REFERENCES users(id);
+ALTER TABLE images 
+  ADD FOREIGN KEY (businessId) REFERENCES businesses(id);
 
 --select (sample_images.id, sample_businesses.name, sample_users.profilename, sample_users.profileurl, sample_users.reviewcount, sample_users.friendcount, sample_images.title, sample_images.description, sample_images.url, sample_images.thumbnail, sample_images.date)
 
--- COPY (select * from images
--- inner join businesses on images.businessid=businesses.id 
--- inner join users on images.userid=users.id) to '/Users/michaelchan/Documents/Repos/W8 SDC/image-carousel/database/cassandra/allData.csv' CSV DELIMITER ',' HEADER;
+-- COPY (
+--   select 
+--   sample_businesses.name as bizname,
+--   sample_users.profilename,
+--   sample_users.profileurl,
+--   sample_users.reviewcount,
+--   sample_users.friendcount,
+--   sample_images.id as imageid,
+--   sample_images.userid as userid,
+--   sample_images.businessId as bizid,
+--   sample_images.title,
+--   sample_images.description,
+--   sample_images.url,
+--   sample_images.thumbnail,
+--   sample_images.date
+-- FROM sample_images inner join sample_businesses on sample_images.businessid=sample_businesses.id 
+-- inner join sample_users on sample_images.userid=sample_users.id) to '/Users/michaelchan/Documents/Repos/8WSDC/image-carousel/database/cassandra/sample.csv' CSV DELIMITER ',' HEADER;
+
+-- COPY (
+--   select 
+--   businesses.name as bizname,
+--   users.profilename,
+--   users.profileurl,
+--   users.reviewcount,
+--   users.friendcount,
+--   images.id as imageid,
+--   images.userid as userid,
+--   images.businessId as bizid,
+--   images.title,
+--   images.description,
+--   images.url,
+--   images.thumbnail,
+--   images.date
+-- FROM images inner join businesses on images.businessid=businesses.id 
+-- inner join users on images.userid=users.id) to '/Users/michaelchan/Documents/Repos/8WSDC/image-carousel/database/cassandra/finalData.csv' CSV DELIMITER ',' HEADER;
+
+
+-- insert into images (userid, businessid, title, description, url, thumbnail, date) VALUES (50, 50, 'fifty', 'fifty', 'www.google.com', 'www.google.com', '2018-04-04')
+
+UPDATE images SET images.title = 'newtitle', images.description = 'updated' WHERE userid=5;
