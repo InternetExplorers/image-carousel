31a751dc7c03e60e4e37632a6d24e2a83b4d5f0e
Michael Chan <mikey.chan1@gmail.com>
2018-08-16 16:37:12 -0700

    reformat server to handle databasehelpers

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const db = require('../database/index');
-const dbHelper = require('../database/dbHelpers.js');
+const db = require('../database/postgreSQL/index');
+const dbHelp = require('../database/postgreSQL/dbhelp');
 
 const app = express();
 const port = 3001;
@@ -11,81 +11,49 @@ app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
   next();
 });
 
 app.use('/:id/', express.static(path.join(__dirname, '/../client/dist')));
 
 app.get('/businesses/:businessId/images', (req, res) => {
-  dbHelper.getImages({ id: req.params.businessId }, (err, images) => {
-    if (err) {
-      res.status(400).send(err);
-    } else {
-      res.status(200).send(images);
-    }
+  dbHelp.getImages({ id: req.params.businessId }, (err, images) => {
+    if (err) res.status(400).send(err);
+    else res.status(200).send(images.rows);
   });
 });
 
-app.post('/businesses/:businessId/images', (req, res) => {
-  dbHelper.postImage({ id: req.params.businessId }, (err, data) => {
-    if (err) {
-      res.status(400).send(err);
-    } else {
-      res.status(200).send();
-    }
-  });
-});
+// app.post('/businesses/:businessId/images', (req, res) => {
+//   dbHelper.postImage({ id: req.params.businessId }, (err, data) => {
+//     if (err) {
+//       res.status(400).send(err);
+//     } else {
+//       res.status(200).send();
+//     }
+//   });
+// });
 
-app.post('/businesses/:businessId/images', (req, res) => {
-  dbHelper.postImage({ id: req.params.businessId }, (err, data) => {
-    if (err) {
-      res.status(400).send(err);
-    } else {
-      res.status(200).send();
-    }
-  });
-});
+// app.put('/businesses/:businessId/images/:imageId', (req, res) => {
+//   const { businessId, imageId } = req.params;
+//   if (err) {
+//     res.status(400).send(err);
+//   } else {
+//     res.status(200).send();
+//   }
+// });
 
-app.put('/businesses/:businessId/images/:imageId', (req, res) => {
-  const { businessId, imageId } = req.params;
-  if (err) {
-    res.status(400).send(err);
-  } else {
-    res.status(200).send();
-  }
-});
-
-app.delete('/businesses/:businessId/images', (req, res) => {
-  const { businessId } = req.params;
-  if (err) {
-    res.status(400).send(err);
-  } else {
-    res.status(200).send();
-  }
-});
-
-app.put('/businesses/:businessId/images/:imageId', (req, res) => {
-  const { businessId, imageId } = req.params;
-  dbHelper.updateImage((err, data) => {
-    if (err) {
-      res.status(400).send(err);
-    } else {
-      res.status(200).send();
-    }
-  })
-});
-
-app.delete('/businesses/:businessId/images', (req, res) => {
-  const { businessId } = req.params;
-  dbHelper.deleteImage({ id: req.params.businessId }, (err, data) => {
-    if (err) {
-      res.status(400).send(err);
-    } else {
-      res.status(200).send();
-    }
-  })
-});
+// app.delete('/businesses/:businessId/images', (req, res) => {
+//   const { businessId } = req.params;
+//   if (err) {
+//     res.status(400).send(err);
+//   } else {
+//     res.status(200).send();
+//   }
+// });
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
