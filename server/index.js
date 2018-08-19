415c2597f231119f05bce50755558f41d84b46e1
Michael Chan <mikey.chan1@gmail.com>
2018-08-18 17:12:46 -0700

    add clustering

--- server/index.js
+++ server/index.js
@@ -1,62 +1,82 @@
 const newrelic = require('newrelic');
-const express = require('express');
 const path = require('path');
+const cluster = require('cluster');
+const express = require('express');
 const dbHelp = require('../database/postgreSQL/dbhelp');
-const app = express();
-const port = 3001;
 
-app.use(express.json());
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
-});
-
-app.use('/:id', express.static(path.join(__dirname, '/../client/dist')));
-
-app.get('/businesses/:businessId/images', (req, res) => {
-  dbHelp.getImages({ id: req.params.businessId }, (err, images) => {
-    if (err) res.json(err);
-    else res.send(images.rows);
+if (cluster.isMaster) {
+  const numCPUs = require('os').cpus().length;
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+  cluster.on('exit', function(worker, code, signal) {
+    console.log(
+      'Worker ' +
+        worker.process.pid +
+        ' died with code: ' +
+        code +
+        ', and signal: ' +
+        signal
+    );
+    cluster.fork();
   });
-});
+} else {
+  const app = express();
+  const port = 3001;
 
-app.post('/businesses/:businessId/images', (req, res) => {
-  dbHelp.postImage(
-    { id: req.params.businessId, body: req.body },
-    (err, data) => {
+  app.use(express.json());
+
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+  });
+
+  app.use('/:id', express.static(path.join(__dirname, '/../client/dist')));
+
+  app.get('/businesses/:businessId/images', (req, res) => {
+    dbHelp.getImages({ id: req.params.businessId }, (err, images) => {
       if (err) res.json(err);
-      else res.json('SENT');
-    }
-  );
-});
+      else res.json(images.rows);
+    });
+  });
 
-app.put('/businesses/:businessid/images/:imageid', (req, res) => {
-  const { businessid, imageid } = req.params;
-  dbHelp.updateImage(
-    { businessid: businessid, imageid: imageid, body: req.body },
-    (err, data) => {
-      if (err) res.json(err);
-      else res.json('UPDATED');
-    }
-  );
-});
+  app.post('/businesses/:businessId/images', (req, res) => {
+    dbHelp.postImage(
+      { id: req.params.businessId, body: req.body },
+      (err, data) => {
+        if (err) res.json(err);
+        else res.json('SENT');
+      }
+    );
+  });
 
-app.delete('/businesses/:businessId/images/:imageid', (req, res) => {
-  const { businessid, imageid } = req.params;
-  dbHelp.deleteImage(
-    { businessid: businessid, imageid: imageid },
-    (err, data) => {
-      if (err) res.json(err);
-      else res.json('DELETED');
-    }
-  );
-});
+  app.put('/businesses/:businessid/images/:imageid', (req, res) => {
+    const { businessid, imageid } = req.params;
+    dbHelp.updateImage(
+      { businessid: businessid, imageid: imageid, body: req.body },
+      (err, data) => {
+        if (err) res.json(err);
+        else res.json('UPDATED');
+      }
+    );
+  });
 
-app.listen(port, () => {
-  console.log(`listening on port ${port}`);
-});
+  app.delete('/businesses/:businessId/images/:imageid', (req, res) => {
+    const { businessid, imageid } = req.params;
+    dbHelp.deleteImage(
+      { businessid: businessid, imageid: imageid },
+      (err, data) => {
+        if (err) res.json(err);
+        else res.json('DELETED');
+      }
+    );
+  });
+  // console.log('Worker %d running!', cluster.worker.id);
+  app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+  });
+}
