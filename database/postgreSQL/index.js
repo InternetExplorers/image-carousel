0e09f2959c755ba65ae2cc57fc1bd2f73c7cadf1
Michael Chan <mikey.chan1@gmail.com>
2018-08-21 15:48:16 -0700

    refactor to use deployed postgres instance

--- database/postgreSQL/index.js
+++ database/postgreSQL/index.js
@@ -1,6 +1,7 @@
 const { Pool, Client } = require('pg');
 
-const connectionString = 'postgres://michaelchan:PASSWORD@localhost:5432/yelp';
+const connectionString =
+  'postgres://power_user:poweruserpassword@ec2-54-153-79-209.us-west-1.compute.amazonaws.com:5432/yelp';
 
 const pool = new Pool({
   connectionString: connectionString,
@@ -14,18 +15,6 @@ const client = new Client({
   connectionString: connectionString,
 });
 client.connect();
-
-// pool.query('SELECT * FROM images WHERE businessid=5', (err, res) => {
-//   if (err) console.log(err);
-//   else console.log(res.rows);
-//   pool.end();
-// });
-
-// client.query('SELECT * FROM images WHERE businessid=5', (err, res) => {
-// console.log(err, res);
-// if (err) console.log(err);
-// else console.log(res.rows);
-// });
 
 module.exports = {
   pool,
